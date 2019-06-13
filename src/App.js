c89d6678194d9c17b647659353ae1ce2745cb7de
Dobrovedov <alexnemerovskii@gmail.com>
2019-06-13 17:21:42 +0300

    v 0.26

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
 import React from 'react';
-import connect from '@vkontakte/vkui-connect';
-import { View } from '@vkontakte/vkui';
+import ReactDOM from 'react-dom';
+import { View, Panel, PanelHeader, Group, List, Cell } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
-
-import Home from './panels/Home';
-import Persik from './panels/Persik';
-
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			activePanel: 'home',
-			fetchedUser: null,
-		};
-	}
-
-	componentDidMount() {
-		connect.subscribe((e) => {
-			switch (e.detail.type) {
-				case 'VKWebAppGetUserInfoResult':
-					this.setState({ fetchedUser: e.detail.data });
-					break;
-				default:
-					console.log(e.detail.type);
-			}
-		});
-		connect.send('VKWebAppGetUserInfo', {});
-	}
-
-	go = (e) => {
-		this.setState({ activePanel: e.currentTarget.dataset.to })
-	};
-
-	render() {
-		return (
-			<View activePanel={this.state.activePanel}>
-				<Home id="home" fetchedUser={this.state.fetchedUser} go={this.go} />
-				<Persik id="persik" go={this.go} />
-			</View>
-		);
-	}
+ 
+function App () {
+  return (
+    <View activePanel="main">
+      <Panel id="main">
+        <PanelHeader>VKUI</PanelHeader>
+        <Group title="Items">
+          <List>
+            <Cell>Hello</Cell>
+            <Cell>World</Cell>
+          </List>
+        </Group>
+      </Panel>
+    </View>
+  );
 }
-
-export default App;
+ 
+ReactDOM.render(<App />, document.getElementById('root'));
