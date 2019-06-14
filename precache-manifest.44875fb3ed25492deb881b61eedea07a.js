d5a95b3839c324c2914c1c2c6105b2f136eb4f7c
Dobrovedov <alexnemerovskii@gmail.com>
2019-06-14 13:02:27 +0300

    Updates

---
+++ precache-manifest.44875fb3ed25492deb881b61eedea07a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5096df5f81105c8a5c15",
+    "url": "/testVK/static/js/runtime~main.5dd316b3.js"
+  },
+  {
+    "revision": "5a75d1021b596d9e200a",
+    "url": "/testVK/static/js/main.2bd18773.chunk.js"
+  },
+  {
+    "revision": "7b3e02ca716b5f7565dc",
+    "url": "/testVK/static/js/2.30754cff.chunk.js"
+  },
+  {
+    "revision": "7b3e02ca716b5f7565dc",
+    "url": "/testVK/static/css/2.6ee5cc31.chunk.css"
+  },
+  {
+    "revision": "ed08a6b1f26f3f1f2d536142801f0f66",
+    "url": "/testVK/index.html"
+  }
+];
