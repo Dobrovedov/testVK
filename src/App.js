4b707f0227f2b2623b9531ea69daa1276c94b7f3
Dobrovedov <alexnemerovskii@gmail.com>
2019-06-15 13:51:51 +0300

    v 0.28

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { View, Panel, PanelHeader, Group, List, Cell, Checkbox, FormLayout, Link } from '@vkontakte/vkui';
+import { View, Panel, PanelHeader, Group, List, Cell, Checkbox, FormLayout, Link, Radio } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
  
 function App () {
   return (
     <View activePanel="main">
       <Panel id="main">
-        <PanelHeader>VKUI</PanelHeader>
-        <Group title="Items">
+        <PanelHeader>Опрос Burger King</PanelHeader>
+        <Group title="вопросы">
           <List>
-            <Cell>Hello</Cell>
-            <Cell>Lipkin</Cell>
-            <Cell>и Нурлан</Cell>
+            <Cell>Просим вас ответить на все вопросы</Cell>
+            <Cell>Это займет всего пару минут</Cell>
           </List>
         </Group>
 	      <FormLayout>
-	        <Checkbox>Я согласен , что я <Link>пидор</Link></Checkbox>
-	        <Checkbox>Я не согласен , что я <Link>пидор</Link></Checkbox>
+	        <Checkbox>Студент</Checkbox>
+	        <Checkbox>Безработный</Checkbox>
+	        <Checkbox>Живу с мамой</Checkbox>
 	      </FormLayout>
+		  <FormLayout>
+        	<div>
+		        <Radio name="radio" value="1" description="Если ты с ИТМО то ставишь это" defaultChecked>Я согласен , что я <Link>пидор</Link></Radio>
+		        <Radio name="radio" value="2">Я не согласен , что я <Link>пидор</Link></Radio>
+        	</div>
+      	  </FormLayout>
       </Panel>
   </View>
 
