7e53c253ed50defd1136b067f39eb17dde3755b5
Andrey <NYanCAtn@yandex.ru>
2019-06-15 14:27:24 +0300

    modified

--- App.js
+++ App.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { View, Panel, PanelHeader, Group, List, Cell, Checkbox, FormLayout, Link } from '@vkontakte/vkui';
+import { View, Panel, PanelHeader, Group, List, Cell, Checkbox, FormLayout, Link, Radio } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
- 
+
+
+
 function App () {
+  
   return (
     <View activePanel="main">
       <Panel id="main">
-        <PanelHeader>VKUI</PanelHeader>
-        <Group title="Items">
+        <PanelHeader>Опрос Burger King</PanelHeader>
+        <Group title="вопросы">
           <List>
-            <Cell>Hello</Cell>
-            <Cell>Lipkin</Cell>
-            <Cell>и Нурлан</Cell>
+            <Cell>Просим вас ответить на все вопросы</Cell>
+            <Cell>Это займет всего пару минут</Cell>
           </List>
         </Group>
-	      
         <FormLayout>
-	        <Checkbox name="check1" checked={this.state.check1}>Я согласен , что я <Link>пидор</Link></Checkbox>
-
-	        <Checkbox name="check2" checked={this.state.check2}>Я не согласен , что я <Link>пидор</Link></Checkbox>
-          Pidor: {this.state.check1} <br />
-          NePidor: {this.state.check2} <br />
-	      </FormLayout>
+          <Checkbox name="check1" checked={this.state.check1}>Студент</Checkbox>
+          <Checkbox name="check2" checked={this.state.check2}>Безработный</Checkbox>
+          <Checkbox name="check1" checked={this.state.check3}>Живу с мамой</Checkbox>
+          Student: {this.state.check1} <br />
+          Homeless: {this.state.check2} <br />
+          Mommy: {this.state.check3} <br />
+        </FormLayout>
+      <FormLayout>
+          <div>
+            <Radio name="radio" value="1" description="Если ты с ИТМО то ставишь это" defaultChecked>Я согласен , что я <Link>пидор</Link></Radio>
+            <Radio name="radio" value="2">Я не согласен , что я <Link>пидор</Link></Radio>
+          </div>
+          </FormLayout>
       </Panel>
   </View>
 
@@ -30,3 +38,5 @@ function App () {
 }
  
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
